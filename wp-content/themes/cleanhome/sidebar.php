7fd69e251a8ee2ed511fd499a0d5530380690268
Scott Bale <drbceb-tech@yahoo.com>
2009-04-01 22:33:04 -0500

    remove subscription section

--- wp-content/themes/cleanhome/sidebar.php
+++ wp-content/themes/cleanhome/sidebar.php
@@ -12,19 +12,6 @@
 		</div>
 
 		<div class="block">
-			<h3>Subscribe</h3>
-				<ul>
-					<?php $url = get_bloginfo("wpurl"); ?>
-					<?php $itunesurl = str_replace('http', 'itpc', $url); ?>
-					<li><a title="Subscripe to all updates"       href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed-icon-14x14.png" /> All updates</a></li>
-					<li><a title="Subscripe to News"                      href="<?php $url; ?>/category/news/feed"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed-icon-14x14.png" /> News</a></li>
-					<li><a title="Subscripe to Articles"                  href="<?php $url; ?>/category/article/feed"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed-icon-14x14.png" /> Articles</a></li>
-					<li><a title="Subscripe to Sermons podcast"           href="<?php $url; ?>/category/sermon/feed"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed-icon-14x14.png" /> Sermons</a></li>
-					<li><a title="Subscripe to Sermons podcast in iTunes" href="<?php echo $itunesurl; ?>/category/sermon/feed"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/iTunes.jpg" /> Sermons (iTunes)</a></li>
-				</ul>
-		</div>
-
-		<div class="block">
 			<h3>Recent Posts</h3>
 				<?php query_posts('showposts=5'); ?>
 				<ul>
