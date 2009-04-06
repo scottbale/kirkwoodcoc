25df23ee1f7fd37280b018bce19bbd2f3cafe7a7
Scott Bale <drbceb-tech@yahoo.com>
2009-04-05 22:19:24 -0500

    make RSS links slightly more obvious

--- wp-content/themes/cleanhome/header.php
+++ wp-content/themes/cleanhome/header.php
@@ -9,8 +9,7 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/reset.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <!--[if IE]><link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/ie.css" type="text/css" media="screen" /><![endif]-->
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> All updates" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Sermons" href="<?php bloginfo('wpurl'); ?>/category/sermon/feed" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Articles" href="<?php bloginfo('wpurl'); ?>/category/article/feed" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> News" href="<?php bloginfo('wpurl'); ?>/category/news/feed" />
