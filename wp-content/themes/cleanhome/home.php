6166886bc8c633c07d2b44319b340851506b72a1
Scott Bale <drbceb-tech@yahoo.com>
2009-04-19 22:27:14 -0500

    hack in gospel meeting banner

--- wp-content/themes/cleanhome/home.php
+++ wp-content/themes/cleanhome/home.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
-	<div class="audio banner">
+	<div class="banner">
+        <div class="bannerline">Please join us for our <a href="/2009/04/15/news/gospel-meeting-april-19-24/">Spring 2008 Gospel Meeting</a> April 19-24</div>
+    </div>
 
+	<div class="banner audiobanner bottomborder">
+        
         
 		<?php if (have_posts()) : ?>
 			<?php $count = 0; ?>
@@ -11,7 +15,7 @@
 				<?php if ( in_category('3') ) : ?>
 					<?php $count++; ?>
 			
-					<div class="audioline">
+					<div class="bannerline">
 						<?php echo get_post_meta( get_the_ID(), 'speaker', true); ?>:
 						<a title="Download the sermon audio" href="<?php the_permalink() ?>"><?php the_title(); ?></a> -
 						<?php echo get_post_meta( get_the_ID(), 'dateRecorded', true); ?>
