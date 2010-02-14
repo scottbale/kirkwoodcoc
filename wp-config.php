2238b4a5fe421e3ee18d2978df2b0278f2420ec7
Scott Bale <drbceb-tech@yahoo.com>
2010-02-14 00:11:26 -0600

    for localhost wp-config, set hostname and remove DB UTF-8 encoding flag

--- wp-config.php
+++ wp-config.php
@@ -28,14 +28,14 @@ define('DB_PASSWORD', 'wp');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', '');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /** uncomment for localhost copy */
-//define('WP_HOME','http://kirkwoodcoc.localhost');
-//define('WP_SITEURL','http://kirkwoodcoc.localhost');
+define('WP_HOME','http://kirkwoodcoc.localhost');
+define('WP_SITEURL','http://kirkwoodcoc.localhost');
 
 /**#@+
  * Authentication Unique Keys.
