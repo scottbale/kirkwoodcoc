7d86b8a45cd05831e9cb06e135b4bfaead0abdee
Scott Bale <scott@balehaus.org>
2014-06-02 21:06:46 -0500

    mo betta phptest.php

--- phptest.php
+++ phptest.php
@@ -8,10 +8,15 @@ php test:
 <?
 echo strpos($_SERVER['REQUEST_URI'], '/wptest');
 if (strpos($_SERVER['REQUEST_URI'], '/wptest') >= 0)  {
-	echo 'yes!' ;
+	echo ' yes!' ;
 } else {
-    echo 'no....' ;
+    echo ' no....' ;
 }
+
+    $conn = mysql_connect('localhost', 'wp', 'wp');
+    if(!$conn) {
+        echo ' Error: ' . mysql_errno() . ' - ' . mysql_error();
+    }
 
 ?>
 </body>
